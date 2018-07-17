1a97d7f9eb2a941d02fda6cb525931ac670b9eab
Leonardo Jorge <leogojorge@outlook.com>
2018-07-17 14:31:51 -0300

    Add project files.

---
+++ TryingNancy.Trying/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryingNancy.Trying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
